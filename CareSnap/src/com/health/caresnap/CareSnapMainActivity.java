1e74491768bcfe7b83d3aeed118bb35ccd118fd9
Paul Cuciureanu <paul@pplconnect.mobi>
2014-02-22 20:00:32 -0500

    first 2 screens

--- CareSnap/src/com/health/caresnap/CareSnapMainActivity.java
+++ CareSnap/src/com/health/caresnap/CareSnapMainActivity.java
@@ -1,23 +1,27 @@
 package com.health.caresnap;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CareSnapMainActivity extends Activity {
+public class CareSnapMainActivity extends Activity implements OnClickListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_care_snap_main);
-    }
+	@Override
+	public void onClick(View v) {
 
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.care_snap_main, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_care_snap_main);
+		findViewById(R.id.button_capture_main).setOnClickListener(this);
+	}
 }
