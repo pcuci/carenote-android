2ce0cb4c53264672cc919e02618e303de56fb723
bvarba <varbanov.b@gmail.com>
2014-03-01 17:56:44 -0500

    added create impression screen, linked few screens together, started implementing simple storage

--- CareSnap/src/com/health/caresnap/CaptureActivity.java
+++ CareSnap/src/com/health/caresnap/CaptureActivity.java
@@ -2,26 +2,42 @@ package com.health.caresnap;
 
 import java.util.ArrayList;
 
-import com.health.caresnap.CaptureSessionGlobal.CaptureSessionState;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.health.caresnap.CaptureSessionGlobal.CaptureSessionState;
 
 public class CaptureActivity extends Activity implements OnClickListener {
 
 	protected static final int REQUEST_OK = 1;
+	protected String impresson;
+	protected Button saveButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_capture);
 		findViewById(R.id.button1).setOnClickListener(this);
+		saveButton = (Button) findViewById(R.id.save_capture_button);
+		saveButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent returnIntent = new Intent();
+				returnIntent.putExtra("impression", impresson);
+				setResult(RESULT_OK, returnIntent);
+				finish();
+			}
+		});
+		saveButton.setEnabled(false);
+
 	}
 
 	@Override
@@ -30,7 +46,7 @@ public class CaptureActivity extends Activity implements OnClickListener {
 		i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
 		try {
 			startActivityForResult(i, REQUEST_OK);
-
+			saveButton.setEnabled(false);
 			updateSessionState(CaptureSessionState.RECORDING);
 		} catch (Exception e) {
 			Toast.makeText(this, "Error initializing speech to text engine.",
@@ -39,14 +55,26 @@ public class CaptureActivity extends Activity implements OnClickListener {
 	}
 
 	@Override
+	public void onBackPressed() {
+		Intent i = new Intent(getBaseContext(), CreateImpressionActivity.class);
+		startActivity(i);
+
+	}
+
+	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (requestCode == REQUEST_OK && resultCode == RESULT_OK) {
 			ArrayList<String> thingsYouSaid = data
 					.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-			((TextView) findViewById(R.id.text1)).setText(thingsYouSaid.get(0));
-
+			impresson = thingsYouSaid.get(0);
+			TextView view = ((TextView) findViewById(R.id.text1));
+			view.setText(impresson);
+			view.setTextSize(24);
 			updateSessionState(CaptureSessionState.PAUSED);
+			saveButton.setEnabled(true);
+			CaptureSessionGlobal global = ((CaptureSessionGlobal) getApplicationContext());
+			global.setRecording(impresson);
 		}
 	}
 
